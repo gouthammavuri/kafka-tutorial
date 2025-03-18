f8c84abfa677236f72355cc8300869cf8cb20956
Goutham Mavuri <goutham2728@gmail.com>
2025-03-17 21:13:50 -0500

    Added the sample producer and consumer for the protobuf schema

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -8,3 +8,14 @@ pip install python-dotenv
 # (Python related dependencies are covered in requirements.txt)
 # echo "Installing OpenAI For Node.js" 
 # npm install --save openai
+
+# Install protoc
+PROTOC_VERSION=3.19.1
+PROTOC_ZIP=protoc-$PROTOC_VERSION-linux-x86_64.zip
+curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOC_VERSION/$PROTOC_ZIP
+unzip -o $PROTOC_ZIP -d protoc
+sudo mv protoc/bin/protoc /usr/local/bin/
+rm -rf protoc $PROTOC_ZIP
+
+# Verify the installation
+protoc --version
